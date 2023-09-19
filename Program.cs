44ea9216da4e679c2881a6c804afeef6a17b1c5e
Toxex <tonyhallentorp@gmail.com>
2023-09-19 18:56:46 +0200

    TruthAboutFalukorv

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 {
     private static void Main()
     {
+        Console.WriteLine("Rikitg falukorv består av 23.2% sågspån");
         AdressBook adressBook = new(new FakeDataManager());
 
         AdressBookMenu menu = new(adressBook);
